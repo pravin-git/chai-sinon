ee7a795035310a0cc9e4c3301404eb2c3a1e09c2
Pravin Pawar <Pravin.Pawar@INFOVISIONLABS.COM>
2020-02-28 16:29:36 +0530

    Initial draft sinon

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,8 @@
 const assert = require("assert");
 const chai = require("chai");
 const expect = chai.expect;
+const sinon = require("sinon");
+const controller = require("../src/controller.js");
 
 describe("smoke test", function () {
     it("checks equality", function () {
@@ -13,3 +15,27 @@ describe("smoke test with chai", function () {
         expect(true).to.be.true;
     });
 });
+
+
+describe("getIndexPage", () =>{
+    it("should return index page", () =>{
+        let req = {}
+        let res = {
+            send: function() {}
+        }
+        controller.getIndexPage(req, res);
+    })
+})
+
+describe("getIndexPage with spy", () =>{
+    it("should return index page with sinon spy", () =>{
+        let req = {}
+        let res = {
+            send: sinon.spy()
+        }
+        controller.getIndexPage(req, res);
+        console.log(res.send);
+        expect(res.send.calledOnce).to.be.true;
+        expect(res.send.firstCall.args[0]).to.equal("Pravin");
+    })
+})
